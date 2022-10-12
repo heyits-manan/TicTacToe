95deb4b190606c901c249e54b125ada08d23b9ae
mananchataut-07 <100475418+mananchataut-07@users.noreply.github.com>
2022-10-12 20:46:15 +0545

    Formatted the code file

--- TicTac.java
+++ TicTac.java
@@ -4,99 +4,81 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class TicTac
-{
+public class TicTac {
 
     static ArrayList<Integer> playerPosition = new ArrayList<Integer>();
     static ArrayList<Integer> cpuPosition = new ArrayList<Integer>();
 
+    public static void main(String[] args) {
+        char[][] gameBoard = {
+                { ' ', '|', ' ', '|', ' ' },
+                { '-', '+', '-', '+', '-' },
+                { ' ', '|', ' ', '|', ' ' },
+                { '-', '+', '-', '+', '-' },
+                { ' ', '|', ' ', '|', ' ' },
+        };
 
-    public static void main(String[] args)
-    {
-     char[][] gameBoard = {
-     {' ','|',' ','|',' '},
-     {'-','+','-','+','-'},
-     {' ','|',' ','|',' '},
-     {'-','+','-','+','-'},
-     {' ','|',' ','|',' '},
-    };
+        printGameBoard(gameBoard);
 
-    printGameBoard(gameBoard);
-    
-    while(true)
-    {
-        
-        
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.println("Entert the position (1 - 9): ");
-        int position = sc.nextInt();
-    String result = checkWinner();
-    if(result.length() > 0)
-    {
-        System.out.println(result);
-        break;
-    }
-    while(playerPosition.contains(position) || cpuPosition.contains(playerPosition) || cpuPosition.contains(position))
-    {
-        System.out.println("Position taken! Please select a new position.");
-        position = sc.nextInt();
-    }
-    
-    placePiece(position, gameBoard,"player");   
-    
-    Random ran = new Random();
-    int cpuPos = ran.nextInt(9) + 1;
-    while(playerPosition.contains(cpuPos) || cpuPosition.contains(cpuPos))
-    {
-        cpuPos = ran.nextInt(9) + 1;
-    }
+        while (true) {
 
-    placePiece(cpuPos, gameBoard,"cpu");   
+            Scanner sc = new Scanner(System.in);
 
- 
-    printGameBoard(gameBoard);
-    result = checkWinner();
-    if(result.length() > 0)
-    {
-        System.out.println(result);
-        break;
-    }
+            System.out.println("Entert the position (1 - 9): ");
+            int position = sc.nextInt();
+            String result = checkWinner();
+            if (result.length() > 0) {
+                System.out.println(result);
+                break;
+            }
+            while (playerPosition.contains(position) || cpuPosition.contains(playerPosition)
+                    || cpuPosition.contains(position)) {
+                System.out.println("Position taken! Please select a new position.");
+                position = sc.nextInt();
+            }
 
-    }
+            placePiece(position, gameBoard, "player");
+
+            Random ran = new Random();
+            int cpuPos = ran.nextInt(9) + 1;
+            while (playerPosition.contains(cpuPos) || cpuPosition.contains(cpuPos)) {
+                cpuPos = ran.nextInt(9) + 1;
+            }
+
+            placePiece(cpuPos, gameBoard, "cpu");
+
+            printGameBoard(gameBoard);
+            result = checkWinner();
+            if (result.length() > 0) {
+                System.out.println(result);
+                break;
+            }
+
+        }
 
     };
 
-
-    public static void printGameBoard(char[][] gameBoard)
-    {
-        for(char[] row: gameBoard)
-        {
-            for(char c: row)
-            {
+    public static void printGameBoard(char[][] gameBoard) {
+        for (char[] row : gameBoard) {
+            for (char c : row) {
                 System.out.print(c);
             }
             System.out.println();
         }
-    }    
+    }
 
-    public static void placePiece(int position, char[][] gameBoard, String user)
-    {
+    public static void placePiece(int position, char[][] gameBoard, String user) {
 
         char symbol = 'X';
-        if(user.equals("player"))
-        {
+        if (user.equals("player")) {
             symbol = 'X';
             playerPosition.add(position);
-        }
-        else if(user.equals("cpu"))
-        {
+        } else if (user.equals("cpu")) {
             symbol = 'O';
             cpuPosition.add(position);
         }
 
-        switch(position)
-        {
+        switch (position) {
             case 1:
                 gameBoard[0][0] = symbol;
                 break;
@@ -126,22 +108,20 @@ public class TicTac
                 break;
             default:
                 break;
-            
+
         }
 
     }
 
-    public static String checkWinner()
-    {
-        List topRow = Arrays.asList(1,2,3);
-        List midRow = Arrays.asList(4,5,6);
-        List bottomRow = Arrays.asList(7,8,9);
-        List leftCol = Arrays.asList(1,4,7);
-        List midCol = Arrays.asList(2,5,8);
-        List rightCol = Arrays.asList(3,6,9);
-        List cross1 = Arrays.asList(1,5,9);
-        List cross2 = Arrays.asList(3,5,7);
-
+    public static String checkWinner() {
+        List topRow = Arrays.asList(1, 2, 3);
+        List midRow = Arrays.asList(4, 5, 6);
+        List bottomRow = Arrays.asList(7, 8, 9);
+        List leftCol = Arrays.asList(1, 4, 7);
+        List midCol = Arrays.asList(2, 5, 8);
+        List rightCol = Arrays.asList(3, 6, 9);
+        List cross1 = Arrays.asList(1, 5, 9);
+        List cross2 = Arrays.asList(3, 5, 7);
 
         List<List> winning = new ArrayList<List>();
         winning.add(topRow);
@@ -153,23 +133,18 @@ public class TicTac
         winning.add(cross1);
         winning.add(cross2);
 
-        for(List l:winning)
-        {
-            if(playerPosition.containsAll(l))
-            {
+        for (List l : winning) {
+            if (playerPosition.containsAll(l)) {
                 return "Congratulations u have won";
-            }
-            else if(cpuPosition.containsAll(l))
-            {
+            } else if (cpuPosition.containsAll(l)) {
                 return "CPU has won";
             }
 
         }
-        if(playerPosition.size() + cpuPosition.size() == 9){
+        if (playerPosition.size() + cpuPosition.size() == 9) {
             return "CAT!";
         }
         return "";
     }
-
 
 }
